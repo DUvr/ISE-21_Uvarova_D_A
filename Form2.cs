f691e35db42cee21673ddda0366ad94d4c784ad3
DUvr <31777703+DUvr@users.noreply.github.com>
2017-10-21 08:38:15 +0400

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba2
+{
+    public partial class Form2 : Form
+    {
+
+
+        Parking parking;
+
+        public Form2()
+        {
+            InitializeComponent();
+            parking = new Parking();
+            Draw();
+        }
+        /// <summary>
+        /// Метод для отрисовки парковки
+        /// </summary>
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxParking.Width, pictureBoxParking.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            parking.Draw(gr, pictureBoxParking.Width, pictureBoxParking.Height);
+            pictureBoxParking.Image = bmp;
+        }
+
+       
+
+        private void buttonSetCar_Click_1(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var car = new Car(100, 4, 10,1600, dialog.Color);
+                int place = parking.PutCarInParking(car);
+                Draw();
+                MessageBox.Show("Ваше место: " + place);
+            }
+
+        }
+
+        private void buttonSetVnedorozhnik_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                ColorDialog dialogDop = new ColorDialog();
+                if (dialogDop.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    var car = new Vnedorozhnik(100, 4, 10, 1600,dialog.Color, true, true, true, dialogDop.Color);
+                    int place = parking.PutCarInParking(car);
+                    Draw();
+                    MessageBox.Show("Ваше место: " + place);
+                }
+            }
+
+        }
+
+        private void buttonTakeCar_Click(object sender, EventArgs e)
+        {
+            if (maskedTextBox1.Text != "")
+            {
+                var car = parking.GetCarInParking(Convert.ToInt32(maskedTextBox1.Text));
+
+                Bitmap bmp = new Bitmap(pictureBoxTakeCar.Width, pictureBoxTakeCar.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                car.setPosition(150, 5);
+                car.drawCar(gr);
+                pictureBoxTakeCar.Image = bmp;
+                Draw();
+            }
+        }
+
+    }
+}
+
---
+++ Parking.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba2
+{
+    class Parking
+    {
+        ClassArray<ITransport> parking;
+        int countPlaces = 20;
+        int placesSizeWidth = 250;
+        int placeSizeHight = 150;
+
+        public Parking()
+        {
+            parking = new ClassArray<ITransport>(countPlaces, null);
+        }
+        public int PutCarInParking(ITransport car)
+        {
+            return parking + car;
+        }
+        public ITransport GetCarInParking(int ticket)
+        {
+            return parking - ticket;
+        }
+        public void Draw(Graphics g,int width, int heigt)
+        {
+            DrawMarking(g);
+            for(int i=0;i<countPlaces;i++)
+            {
+                var car = parking.getObject(i);
+                if (car!=null)
+                {
+                    car.setPosition(5 + i / 5 * placesSizeWidth + 25, i % 5 * placeSizeHight + 15);
+                    car.drawCar(g);
+                }
+            }
+        }
+        private void DrawMarking(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black, 3);
+            g.DrawRectangle(pen, 0, 0, (countPlaces / 2) * placeSizeHight, 600);
+            g.DrawLine(pen, 0, placeSizeHight,50, placeSizeHight);
+            g.DrawLine(pen, 0, placeSizeHight+150, 50, placeSizeHight+150);
+            g.DrawLine(pen, 0, placeSizeHight+300, 50, placeSizeHight+300);
+            for (int i=0;i<countPlaces/5;i++)
+            {
+                for (int j=0;j<6;j++)
+                {
+                    g.DrawLine(pen, i * placesSizeWidth+20, j * placeSizeHight,i * placesSizeWidth + 150, j * placeSizeHight);
+                    
+                }
+                
+                g.DrawLine(pen, i * placesSizeWidth+270, 0, i * placesSizeWidth+270, 700);
+            }
+        }
+    }
+}
